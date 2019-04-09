5edc73877299e2d91d19a1ae8e17803064b7a955
akpati <amit.pati25@gmail.com>
2019-04-09 22:36:40 +0530

    changes

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var request = require("request");
+const port = process.env.PORT || 3030; 
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+
+app.listen(port, function (error) {
+    if (error) {
+        console.error(error)
+    } else {
+        console.info("Listening on port %s. Open up http://localhost:%s/ in your browser.", port, port)
+    }
+})
